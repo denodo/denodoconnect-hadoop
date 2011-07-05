78dd6a5f62dc3c5dcca87193fe9a9da2afa7fbb6
sg <sg.redacted@denodo.com>
2011-07-05 12:50:53 +0000

    Project start. Need improvements and generalization

---
+++ src/main/java/com/denodo/devkit/hadoop/exceptions/UnsupportedProjectionException.java
@@ -0,0 +1,12 @@
+package com.denodo.devkit.hadoop.exceptions;
+
+public class UnsupportedProjectionException extends RuntimeException {
+
+	private static final long serialVersionUID = 3944705265055906122L;
+	
+	public UnsupportedProjectionException(String message) {
+		super(message);
+	}
+	
+
+}
