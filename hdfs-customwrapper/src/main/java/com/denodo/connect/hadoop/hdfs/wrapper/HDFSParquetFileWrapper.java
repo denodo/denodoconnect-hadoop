9fada4121f0a73fec893c1dbe5f6bde7f91ed276
ll <ll.redacted@denodo.com>
2019-09-11 18:04:23 +0200

    - Add where delegations in HDFSParquetFileWrapper

--- hdfs-customwrapper/src/main/java/com/denodo/connect/hadoop/hdfs/wrapper/HDFSParquetFileWrapper.java
+++ hdfs-customwrapper/src/main/java/com/denodo/connect/hadoop/hdfs/wrapper/HDFSParquetFileWrapper.java
@@ -21,39 +21,42 @@
  */
 package com.denodo.connect.hadoop.hdfs.wrapper;
 
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Types;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+import com.denodo.vdb.engine.customwrapper.condition.*;
+import com.denodo.vdb.engine.customwrapper.expression.CustomWrapperExpression;
+import com.denodo.vdb.engine.customwrapper.expression.CustomWrapperSimpleExpression;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.filter2.compat.FilterCompat;
+import org.apache.parquet.filter2.predicate.FilterPredicate;
 import org.apache.parquet.hadoop.ParquetInputFormat;
+import org.apache.parquet.io.api.Binary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.denodo.connect.hadoop.hdfs.commons.naming.Parameter;
 import com.denodo.connect.hadoop.hdfs.commons.schema.SchemaElement;
 import com.denodo.connect.hadoop.hdfs.reader.HDFSParquetFileReader;
-import com.denodo.connect.hadoop.hdfs.util.configuration.HadoopConfigurationUtils;
 import com.denodo.connect.hadoop.hdfs.util.schema.VDPSchemaUtils;
 import com.denodo.vdb.engine.customwrapper.CustomWrapperConfiguration;
 import com.denodo.vdb.engine.customwrapper.CustomWrapperException;
 import com.denodo.vdb.engine.customwrapper.CustomWrapperInputParameter;
 import com.denodo.vdb.engine.customwrapper.CustomWrapperResult;
 import com.denodo.vdb.engine.customwrapper.CustomWrapperSchemaParameter;
-import com.denodo.vdb.engine.customwrapper.condition.CustomWrapperConditionHolder;
 import com.denodo.vdb.engine.customwrapper.expression.CustomWrapperFieldExpression;
 import com.denodo.vdb.engine.customwrapper.input.type.CustomWrapperInputParameterTypeFactory;
 import com.denodo.vdb.engine.customwrapper.input.type.CustomWrapperInputParameterTypeFactory.RouteType;
-import com.denodo.vdb.engine.customwrapper.input.value.CustomWrapperInputParameterRouteValue;
-import com.denodo.vdb.engine.customwrapper.input.value.CustomWrapperInputParameterValue;
 
+import static com.denodo.vdb.engine.customwrapper.condition.CustomWrapperCondition.*;
 import static org.apache.parquet.filter2.predicate.FilterApi.*;
 
 /**
@@ -100,9 +103,14 @@ public class HDFSParquetFileWrapper extends AbstractSecureHadoopWrapper {
     @Override
     public CustomWrapperConfiguration getConfiguration() {
 
-        final CustomWrapperConfiguration conf = super.getConfiguration();
-
-        return conf;
+        final CustomWrapperConfiguration configuration = super.getConfiguration();
+        configuration.setDelegateProjections(true);
+        configuration.setDelegateOrConditions(true);
+        configuration.setAllowedOperators(new String[] {
+            OPERATOR_EQ, OPERATOR_NE, OPERATOR_LT, OPERATOR_LE,
+            OPERATOR_GT, OPERATOR_GE
+        });
+        return configuration;
     }
 
     @Override
@@ -125,7 +133,7 @@ public class HDFSParquetFileWrapper extends AbstractSecureHadoopWrapper {
 
             final SchemaElement javaSchema = reader.getSchema(conf);
             if(includePathColumn){
-                final CustomWrapperSchemaParameter filePath = new CustomWrapperSchemaParameter(Parameter.FULL_PATH, Types.VARCHAR, null, false,
+                final CustomWrapperSchemaParameter filePath = new CustomWrapperSchemaParameter(Parameter.FULL_PATH, Types.VARCHAR, null, true,
                     CustomWrapperSchemaParameter.NOT_SORTABLE, false, true, false);
                 return (CustomWrapperSchemaParameter[]) ArrayUtils.add(VDPSchemaUtils.buildSchemaParameterParquet(javaSchema.getElements()),filePath);
             }else {
@@ -162,8 +170,12 @@ public class HDFSParquetFileWrapper extends AbstractSecureHadoopWrapper {
         final String parquetFilePath = inputValues.get(Parameter.PARQUET_FILE_PATH);
         final Path path = new Path(parquetFilePath);
 
-        /*ParquetInputFormat.setFilterPredicate(conf, lt(intColumn("id"),5));
-        FilterCompat.Filter filter = ParquetInputFormat.getFilter(conf);*/
+        FilterPredicate filterPredicate = this.buildFilter(condition.getComplexCondition());
+        FilterCompat.Filter filter = null;
+        if (filterPredicate != null) {
+            ParquetInputFormat.setFilterPredicate(conf,filterPredicate);
+            filter = ParquetInputFormat.getFilter(conf);
+        }
 
         final String fileNamePattern = inputValues.get(Parameter.FILE_NAME_PATTERN);
 
@@ -172,7 +184,7 @@ public class HDFSParquetFileWrapper extends AbstractSecureHadoopWrapper {
         HDFSParquetFileReader reader = null;
         try {
 
-            reader = new HDFSParquetFileReader(conf, path, fileNamePattern, null, projectedFields, includePathColumn, false, null);
+            reader = new HDFSParquetFileReader(conf, path, fileNamePattern, null, projectedFields, includePathColumn, false, filter);
 
             Object parquetData = reader.read();
             while (parquetData != null && !isStopRequested()) {
@@ -197,6 +209,188 @@ public class HDFSParquetFileWrapper extends AbstractSecureHadoopWrapper {
             }
 
         }
+    }
+
+    public FilterPredicate buildFilter (final CustomWrapperCondition vdpCondition) throws CustomWrapperException {
+
+        if (vdpCondition != null) {
+            if (vdpCondition.isAndCondition()) {
+                CustomWrapperAndCondition andCondition = (CustomWrapperAndCondition) vdpCondition;
+                List<FilterPredicate> filterPredicates  = new ArrayList<FilterPredicate>();
+                for (CustomWrapperCondition condition : andCondition.getConditions()) {
+                    if (condition.isSimpleCondition()) {
+                        FilterPredicate filterPredicate = this.generateSimpleFilterPredicate(condition);
+                        filterPredicates.add(filterPredicate);
+                    } else {
+                        FilterPredicate filterPredicateComplex = this.buildFilter(condition);
+                        filterPredicates.add(filterPredicateComplex);
+                    }
+                }
+                if (filterPredicates.size() >= 2) {
+                    FilterPredicate filterPredicate = filterPredicates.get(0);
+                    for (int i = 1; i < filterPredicates.size(); i++) {
+                        filterPredicate = and(filterPredicate,filterPredicates.get(i));
+                    }
+                    return  filterPredicate;
+                } else {
+                    throw new CustomWrapperException("Error obtaining the FilterPredicate for the and condition \"" + andCondition.toString() + "\"");
+                }
+            } else if (vdpCondition.isOrCondition()) {
+                CustomWrapperOrCondition orCondition = (CustomWrapperOrCondition) vdpCondition;
+                List<FilterPredicate> filterPredicates  = new ArrayList<FilterPredicate>();
+                for (CustomWrapperCondition condition : orCondition.getConditions()) {
+                    if (condition.isSimpleCondition()) {
+                        FilterPredicate filterPredicate = this.generateSimpleFilterPredicate(condition);
+                        filterPredicates.add(filterPredicate);
+                    } else {
+                        FilterPredicate filterPredicateComplex = this.buildFilter(condition);
+                        filterPredicates.add(filterPredicateComplex);
+                    }
+                }
+                if (filterPredicates.size() >= 2) {
+                    FilterPredicate filterPredicate = filterPredicates.get(0);
+                    for (int i = 1; i < filterPredicates.size(); i++) {
+                        filterPredicate = or(filterPredicate,filterPredicates.get(i));
+                    }
+                    return  filterPredicate;
+                } else {
+                    throw new CustomWrapperException("Error obtaining the FilterPredicate for the and condition \"" + orCondition.toString() + "\"");
+                }
+            } else if (vdpCondition.isSimpleCondition()) {
+                return this.generateSimpleFilterPredicate(vdpCondition);
+            } else {
+                throw new CustomWrapperException("Condition \"" + vdpCondition.toString() + "\" not allowed");
+            }
+
+        } else {
+            return null;
+        }
+        /*ParquetInputFormat.setFilterPredicate(conf, lt(intColumn("id"),5));
+        FilterCompat.Filter filter = ParquetInputFormat.getFilter(conf);*/
+    }
+
+    private FilterPredicate generateSimpleFilterPredicate(CustomWrapperCondition condition) {
+
+        CustomWrapperSimpleCondition simpleCondition = (CustomWrapperSimpleCondition) condition;
+        String operator = simpleCondition.getOperator();
+        FilterPredicate filterPredicate = null;
+
+        String field = simpleCondition.getField().toString();
+        for (CustomWrapperExpression expression : simpleCondition.getRightExpression()) {
+            if (expression.isSimpleExpression()) {
+                CustomWrapperSimpleExpression simpleExpression = (CustomWrapperSimpleExpression)expression;
+                switch (operator) {
+                    case CustomWrapperCondition.OPERATOR_EQ:
+                        if (simpleExpression.getValue() instanceof Integer) {
+                            filterPredicate = eq(intColumn(field),Integer.parseInt(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Long) {
+                            filterPredicate = eq(longColumn(field),Long.parseLong(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Double) {
+                            filterPredicate = eq(doubleColumn(field),Double.parseDouble(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Float) {
+                            filterPredicate = eq(floatColumn(field),Float.parseFloat(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof String) {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = eq(binaryColumn(field),Binary.fromString(s));
+                        } else if (simpleExpression.getValue() instanceof Boolean) {
+                            filterPredicate = eq(booleanColumn(field),Boolean.valueOf(simpleExpression.getValue().toString()));
+                        } else {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = eq(binaryColumn(field),Binary.fromString(s));
+                        }
+                        break;
+                    case CustomWrapperCondition.OPERATOR_NE:
+                        if (simpleExpression.getValue() instanceof Integer) {
+                            filterPredicate = notEq(intColumn(field),Integer.parseInt(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Long) {
+                            filterPredicate = notEq(longColumn(field),Long.parseLong(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Double) {
+                            filterPredicate = notEq(doubleColumn(field),Double.parseDouble(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Float) {
+                            filterPredicate = notEq(floatColumn(field),Float.parseFloat(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof String) {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = notEq(binaryColumn(field),Binary.fromString(s));
+                        } else if (simpleExpression.getValue() instanceof Boolean) {
+                            filterPredicate = notEq(booleanColumn(field),Boolean.valueOf(simpleExpression.getValue().toString()));
+                        } else {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = notEq(binaryColumn(field),Binary.fromString(s));
+                        }
+                        break;
+                    case CustomWrapperCondition.OPERATOR_LT:
+                        if (simpleExpression.getValue() instanceof Integer) {
+                            filterPredicate = lt(intColumn(field),Integer.parseInt(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Long) {
+                            filterPredicate = lt(longColumn(field),Long.parseLong(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Double) {
+                            filterPredicate = lt(doubleColumn(field),Double.parseDouble(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Float) {
+                            filterPredicate = lt(floatColumn(field),Float.parseFloat(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof String) {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = lt(binaryColumn(field),Binary.fromString(s));
+                        } else {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = lt(binaryColumn(field),Binary.fromString(s));
+                        }
+                        break;
+                    case CustomWrapperCondition.OPERATOR_LE:
+                        if (simpleExpression.getValue() instanceof Integer) {
+                            filterPredicate = ltEq(intColumn(field),Integer.parseInt(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Long) {
+                            filterPredicate = ltEq(longColumn(field),Long.parseLong(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Double) {
+                            filterPredicate = ltEq(doubleColumn(field),Double.parseDouble(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Float) {
+                            filterPredicate = ltEq(floatColumn(field),Float.parseFloat(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof String) {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = ltEq(binaryColumn(field),Binary.fromString(s));
+                        } else {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = ltEq(binaryColumn(field),Binary.fromString(s));
+                        }
+                        break;
+                    case CustomWrapperCondition.OPERATOR_GT:
+                        if (simpleExpression.getValue() instanceof Integer) {
+                            filterPredicate = gt(intColumn(field),Integer.parseInt(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Long) {
+                            filterPredicate = gt(longColumn(field),Long.parseLong(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Double) {
+                            filterPredicate = gt(doubleColumn(field),Double.parseDouble(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Float) {
+                            filterPredicate = gt(floatColumn(field),Float.parseFloat(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof String) {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = gt(binaryColumn(field),Binary.fromString(s));
+                        } else {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = gt(binaryColumn(field),Binary.fromString(s));
+                        }
+                        break;
+                    case CustomWrapperCondition.OPERATOR_GE:
+                        if (simpleExpression.getValue() instanceof Integer) {
+                            filterPredicate = gtEq(intColumn(field),Integer.parseInt(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Long) {
+                            filterPredicate = gtEq(longColumn(field),Long.parseLong(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Double) {
+                            filterPredicate = gtEq(doubleColumn(field),Double.parseDouble(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof Float) {
+                            filterPredicate = gtEq(floatColumn(field),Float.parseFloat(simpleExpression.getValue().toString()));
+                        } else if (simpleExpression.getValue() instanceof String) {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = gtEq(binaryColumn(field),Binary.fromString(s));
+                        } else {
+                            String s = simpleExpression.getValue().toString();
+                            filterPredicate = gtEq(binaryColumn(field),Binary.fromString(s));
+                        }
+                        break;
+                    default:
+                }
+            }
+        }
+        return filterPredicate;
     }
 
 }
